c6ec1ae30d04e52d280c41ab21b563d382cbd0fa
PramodSolapur <solapurpramod2@gmail.com>
2023-09-27 16:13:33 +0530

    ready for production

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ cloudinary.config({
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 // GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
@@ -44,7 +44,7 @@ app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
